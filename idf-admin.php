5d4cb33a5cf36a400c43bf7ee76c6586ce40a3fd
nhangen <nhangen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-05 18:28:20 +0000

    commit IDF 1.0, tag it, and remove ID 1.3.9 tag.
    Update readme.
    
    git-svn-id: https://plugins.svn.wordpress.org/ignitiondeck/tags/1.0@927281 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ idf-admin.php
@@ -0,0 +1,76 @@
+<?php
+add_action('admin_menu', 'idf_admin_menus');
+
+function idf_admin_menus() {
+	if (current_user_can('manage_options')) {
+		$home = add_menu_page(__('IgnitionDeck', 'idf'), __('IgnitionDeck', 'idf'), 'manage_options', 'idf', 'idf_main_menu', plugins_url( '/idf/images/ignitiondeck-menu.png', DIRNAME(__FILE__) ));
+		$theme_list = add_submenu_page( 'idf', __('Themes', 'ignitiondeck'), __('Themes', 'ignitiondeck'), 'manage_options', 'idf-themes', 'idf_theme_list');
+		$extension_list = add_submenu_page( 'idf', __('Extensions', 'ignitiondeck'), __('Extensions', 'ignitiondeck'), 'manage_options', 'idf-extensions', 'idf_extension_list');
+		$menu_array = array($home,
+						$theme_list,
+						$extension_list
+						);
+		foreach ($menu_array as $menu) {
+			add_action('admin_print_styles-'.$menu, 'idf_admin_enqueues');
+		}
+	}
+}
+
+function idf_main_menu() {
+	$idf_registered = get_option('idf_registered');
+	$platform = get_option('idf_commerce_platform', true);
+	if (isset($_POST['commerce_submit'])) {
+		$platform = esc_attr($_POST['commerce_selection']);
+		update_option('idf_commerce_platform', $platform);
+	}
+	include_once 'templates/admin/_idfMenu.php';
+}
+
+function idf_extension_list() {
+	$plugins = get_plugins();
+	/*$plugin_array = array();
+	if (!empty($plugins)) {
+		foreach ($plugins as $plugin) {
+			$plugin_array[] = $plugin['basename'];
+		}
+	}*/
+	$api = 'http://ignitiondeck.com/id/?action=get_extensions';
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $api);
+
+	$json = curl_exec($ch);
+	curl_close($ch);
+	$data = json_decode($json);
+	include_once 'templates/admin/_extensionList.php';
+}
+
+function idf_theme_list() {
+	$themes = wp_get_themes();
+	$name_array = array();
+	if (!empty($themes)) {
+		foreach ($themes as $theme) {
+			$name_array[] = $theme->Name;
+		}
+	}
+	$active_theme = wp_get_theme();
+	$active_name = $active_theme->Name;
+	$api = 'http://ignitiondeck.com/id/?action=get_themes';
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $api);
+
+	$json = curl_exec($ch);
+	curl_close($ch);
+	$data = json_decode($json);
+	include_once 'templates/admin/_themeList.php';
+}
+
+function idf_admin_enqueues() {
+	wp_register_script('idf-admin', plugins_url('/js/idf-admin.js', __FILE__));
+	wp_register_style('idf-admin', plugins_url('/css/idf-admin.css', __FILE__));
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('idf-admin');
+	wp_enqueue_style('idf-admin');
+}
+?>
---
+++ idf-functions.php
@@ -0,0 +1,63 @@
+<?php
+function idf_platform() {
+	$platform = get_option('idf_commerce_platform', true);
+	return $platform;
+}
+
+function idf_registered() {
+	/*
+	1. Set option with any login info we need and keep logged in
+	2. Download ID and 500
+	3. Offer to activate ID and 500
+	*/
+	// 1
+	update_option('idf_registered', 1);
+	// 2 (we should use an API to deliver latest and not hard code url)
+	$plugins_path = plugin_dir_path(dirname(__FILE__));
+	$themes_path = plugin_dir_path(dirname(dirname(__FILE__))).'themes/';
+	$idcf = file_get_contents('http://ignitiondeck.com/idf/idcf_latest.zip');
+	$fh = file_get_contents('http://ignitiondeck.com/idf/fh_latest.zip');
+	if (!empty($idcf)) {
+		$put_idcf = file_put_contents($plugins_path.'idcf_latest.zip', $idcf);
+		$idcf_zip = new ZipArchive;
+		$idcf_zip_res = $idcf_zip->open($plugins_path.'idcf_latest.zip');
+		if ($idcf_zip_res) {
+			if (!file_exists($plugins_path.'ignitiondeck')) {
+				$idcf_zip->extractTo($plugins_path);
+				$idcf_zip->close();
+			}
+			unlink($plugins_path.'idcf_latest.zip');
+		}
+	}
+	if (!empty($fh)) {
+		$put_fh = file_put_contents($themes_path.'fh_latest.zip', $fh);
+		$fh_zip = new ZipArchive;
+		$fh_zip_res = $fh_zip->open($themes_path.'fh_latest.zip');
+		if ($fh_zip_res) {
+			if (!file_exists($themes_path.'fivehundred')) {
+				$fh_zip->extractTo($themes_path);
+				$fh_zip->close();
+			}
+			unlink($themes_path.'fh_latest.zip');
+		}
+	}
+	// 3
+	activate_plugin($plugins_path.'ignitiondeck/ignitiondeck.php');
+}
+
+add_action('wp_ajax_idf_registered', 'idf_registered');
+add_action('wp_ajax_nopriv_idf_registered', 'idf_registered');
+
+function idf_activate_theme() {
+	if (isset($_POST['theme'])) {
+		$slug = esc_attr($_POST['theme']);
+		$slug = str_replace('500', 'fivehundred', $slug);
+		switch_theme($slug);
+		echo 1;
+	}
+	exit;
+}
+
+add_action('wp_ajax_idf_activate_theme', 'idf_activate_theme');
+add_action('wp_ajax_nopriv_idf_activate_theme', 'idf_activate_theme');
+?>
